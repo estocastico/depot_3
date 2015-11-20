9af0d3357fa152ac00db9cc1d525a165f5f649c9
alejo <alejorodasvasquez@gmail.com>
2015-11-20 11:00:45 -0500

    se elimina columna cart_id #iteration_D3

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151120144042) do
+ActiveRecord::Schema.define(version: 20151120155613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 20151120144042) do
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "cart_id"
   end
 
   create_table "line_items", force: :cascade do |t|
